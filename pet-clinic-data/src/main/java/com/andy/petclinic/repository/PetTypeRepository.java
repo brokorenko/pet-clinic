690d3f385de3ffd2ba966f75298652909b418568
brokorenko <brokorenko@gmail.com>
2018-11-02 15:05:51 +0300

    spring data jpa service for petType added. closes #52

--- pet-clinic-data/src/main/java/com/andy/petclinic/repository/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/com/andy/petclinic/repository/PetTypeRepository.java
@@ -1,7 +1,7 @@
 package com.andy.petclinic.repository;
 
-import com.andy.petclinic.model.Pet;
+import com.andy.petclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetTypeRepository extends CrudRepository<Pet, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
